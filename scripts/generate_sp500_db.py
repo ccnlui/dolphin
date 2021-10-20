cc5cd5dbb843e83d6b44d2bb83a3aa49131ab93f
calvin <ccnlui@gmail.com>
2021-10-20 00:06:39 -0400

    Complete script to generate sp500 database.

--- scripts/generate_sp500_db.py
+++ scripts/generate_sp500_db.py
@@ -23,34 +23,6 @@ CREDENTIALS_FULLPATH = "/media/calvin/Backup Plus/backup/source/credentials/cred
 #------------------------------------------------------------------------------
 def generate_sp500_database(sp500_data_fullpath, overwrite=False):
 
-    #------------------------------------------------------------------------------
-    # Generate table data.
-    #------------------------------------------------------------------------------
-    df_sp500 = pd.read_csv(sp500_data_fullpath)
-
-    intervals = defaultdict(list)
-    start_dates = {}
-
-    for row in df_sp500.itertuples():
-        next_date = row.date
-        tickers = row.tickers.split(',')
-        next_tickers = set(tickers)
-        curr_tickers = set(start_dates.keys())
-
-        # Interval ends.
-        for symbol in curr_tickers - next_tickers:
-            curr_date = start_dates[symbol]
-            intervals[symbol].append([curr_date, next_date])
-            start_dates.pop(symbol)
-
-        # Interval starts.
-        for symbol in next_tickers - curr_tickers:
-            start_dates[symbol] = next_date
-
-    # Current sp500 members.
-    for symbol, start_date in start_dates.items():
-        intervals[symbol].append([start_date, None])
-
     #--------------------------------------------------------------------------
     # Connect to MariaDB.
     #--------------------------------------------------------------------------
@@ -105,7 +77,57 @@ def generate_sp500_database(sp500_data_fullpath, overwrite=False):
     """
     cursor.execute(sql_query)
 
-    pass
+    #------------------------------------------------------------------------------
+    # Generate table data.
+    #------------------------------------------------------------------------------
+    df_sp500 = pd.read_csv(sp500_data_fullpath)
+
+    intervals = defaultdict(list)
+    start_dates = {}
+
+    for row in df_sp500.itertuples():
+        next_date = row.date
+        tickers = row.tickers.split(',')
+        next_tickers = set(tickers)
+        curr_tickers = set(start_dates.keys())
+
+        # Interval ends.
+        for symbol in curr_tickers - next_tickers:
+            curr_date = start_dates[symbol]
+            intervals[symbol].append([curr_date, next_date])
+            start_dates.pop(symbol)
+
+        # Interval starts.
+        for symbol in next_tickers - curr_tickers:
+            start_dates[symbol] = next_date
+
+    # Current sp500 members.
+    for symbol, start_date in start_dates.items():
+        intervals[symbol].append([start_date, None])
+
+    #------------------------------------------------------------------------------
+    # Insert into database.
+    #------------------------------------------------------------------------------
+    values = []
+    for symbol, itvs in intervals.items():
+        for itv in itvs:
+            values.append((symbol, itv[0], itv[1]))
+    values.sort()
+
+    sql_query = """
+        INSERT INTO membership (symbol, start_date, end_date)
+        VALUES (%s, %s, %s)
+    """
+
+    try:
+        cursor.executemany(sql_query, values)
+    except:
+        print("[ERROR] Cannot insert into database: {} {}".format(sql_query, df_symbol_values))
+        raise
+
+    connection.commit()
+
+    return
 
 
 #------------------------------------------------------------------------------
